14d5364ddc5d4d577bf0bc43a09a9e50baa695b5
John P <70922864+p134c0d3@users.noreply.github.com>
2023-10-04 22:14:38 -0500

    Sidebar, Dashboard Work
    - Completed most of sidebar except for email at bottom
    - Created dashboard title, filter buttons

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  isStatusExpanded: boolean = false;
+  isDateExpanded: boolean = false;
+  isPriorityExpanded: boolean = false;
 }
